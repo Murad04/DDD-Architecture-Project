b0bb2999f09f1fa5aa042b6519be8add7773ba32
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-10 23:20:36 +0400

    Added interfaces to Domain\Common\Entities

---
+++ Domain/Common/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common.Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Entities
+{
+    public abstract class BaseEntity<T> : IEntity
+    {
+        public T Id { get; set; }
+    }
+}
---
+++ Domain/Common/Entities/Interfaces/IAudited.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Entities.Interfaces
+{
+    public interface IAudited
+    {
+    }
+
+    public interface IHasCreationTime : IAudited
+    {
+        public DateTime CreateTime { get; set; }
+    }
+
+}
