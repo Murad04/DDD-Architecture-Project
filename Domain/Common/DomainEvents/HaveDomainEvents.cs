8d42abbac22937d85ee97b0e896a6412f67ba03c
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-11 12:30:21 +0400

    Added events and repository

---
+++ Domain/Common/DomainEvents/HaveDomainEvents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.DomainEvents
+{
+    public abstract class HaveDomainEvents
+    {
+        public List<BaseDomainEvent> Events = new List<BaseDomainEvent>();
+    }
+}
---
+++ Domain/Common/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common.Repository.Interfaces
+{
+    public interface IRepository<TEntity, in TPrimaryKey> :IDisposable where TEntity : BaseEntity<TPrimaryKey>   
+    {
+        IQueryable<TEntity> GetAll();
+        
+        IQueryable<TEntity> GetAllIncluding(params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<List<TEntity>> GetAllList();
+
+        Task<List<TEntity>> GetAllListIncluding(params Expression<Func<TEntity, object>>[] includeProperties);
+
+        ValueTask<TEntity> Find(TPrimaryKey id);
+
+        Task<TEntity> GetFirst(TPrimaryKey id);
+
+        Task<TEntity> GetFirstIncluding(TPrimaryKey id,params Expression<Func<TEntity, object>>[] includeProperties);
+
+        Task<TEntity> GetFirst(Expression<Func<TEntity, bool>> predicate);
+
+        //Task<TEntity> GetFirstIncluding(Expression<Func>)
+
+    }
+}
