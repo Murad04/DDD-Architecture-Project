77b1e6b776b04727fd7d2a559872a55fcef66b6e
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-14 17:44:56 +0400

    Added configurations to Program.cs

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,13 @@
+using Application;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain;
+using FluentValidation.AspNetCore;
+using Infrastructure;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using Web.Common;
+using Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,22 +19,55 @@ builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddDomain();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddControllers()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining(typeof(ValidationException)))
+    .AddNewtonsoftJson();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo
+{
+    Title = "DDD.CleanArchitecture template API",
+    Version = "v1"
+}));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+    app.UseDatabaseErrorPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCustomExceptionHandler();
+
+app.UseHealthChecks("/health");
+
+app.UseRouting();
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DDD.CleanArchitecture template API V1"));
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
