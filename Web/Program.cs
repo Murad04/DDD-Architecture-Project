46da4c6ddf5307e2298bda394c33976e3ed3e7f7
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-14 13:48:02 +0400

    Implemented Serilog and currentuserservice

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,7 +1,15 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adding serilog
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ Web/Services/CurrentUserService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Microsoft.Extensions.Primitives;
+using System.Text;
+
+namespace Web.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            if (long.TryParse(GetHeaderValue(httpContextAccessor, "X-UserID"), out long userID))
+            {
+                UserID = UserID;
+            }
+        }
+
+        public long? UserID { get;}
+
+        private string GetHeaderValue(IHttpContextAccessor httpContextAccessor,string headerKey)
+        {
+            if(httpContextAccessor.HttpContext!=null && httpContextAccessor.HttpContext.Request.Headers.TryGetValue(headerKey,out StringValues headerValues))
+            {
+                return headerValues.First();
+            }
+            return String.Empty;
+        }
+    }
+}
