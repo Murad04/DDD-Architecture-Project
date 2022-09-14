045b88d3f9a6ac1a767336b553d303a7da890012
Murad <67361462+Murad04@users.noreply.github.com>
2022-09-14 12:40:56 +0400

    Modified TaskController with MediatR

--- Web/Controllers/TaskController.cs
+++ Web/Controllers/TaskController.cs
@@ -1,6 +1,45 @@
-﻿namespace Web.Controllers
+﻿using Application.Tasks.Commands.CreateTask;
+using Application.Tasks.Commands.DeleteTask;
+using Application.Tasks.Commands.UpdateTask;
+using Application.Tasks.Queries.GetTasks;
+using Application.Tasks.Queries.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
 {
-    public class TaskController
+    public class TaskController:ApiController
     {
+        [HttpGet]
+        public async Task<ActionResult<TaskView>> Get()
+        {
+            return await Mediator.Send(new GetTasksQuery());
+        }
+
+        [HttpGet("{taskName}")]
+        public async Task<ActionResult<TaskView>> GetByName(string taskName)
+        {
+            return await Mediator.Send(new GetTasksQuery() { Name = taskName });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateTaskCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+
+        [HttpPut("{taskID}")]
+        public async Task<ActionResult> Update(int taskID,UpdateTaskCommand command)
+        {
+            if (taskID != command.ID) return BadRequest();
+            await Mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{taskID}")]
+        public async Task<ActionResult> Delete(int taskID,DeleteTaskCommand command)
+        {
+            await Mediator.Send(new DeleteTaskCommand { ID = taskID });
+            return NoContent();
+        }
     }
 }
